9a5f6fe6b2fb30065123f198e3fa3da5f2f236dc
yangaofeng <yangaofeng@ehomepay.com.cn>
2018-04-30 21:09:20 +0800

    teach python

---
+++ 文件操作/file01.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+f = open('test.txt','r')
+print (f.readline())
+print (f.readline()) #当文件较大时需要使用 readline操作文件，相当与一个迭代器
+
+
+for line in f:
+    print('adsfjkas',line)
+
+f.close()
