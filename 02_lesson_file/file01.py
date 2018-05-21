7d09e1973dca0b616be347f29c7e44060fd2102b
yangaofeng <yangaofeng@ehomepay.com.cn>
2018-05-21 14:23:08 +0800

    function

--- 02_lesson_file/file01.py
+++ 02_lesson_file/file01.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python
 f = open('test.txt', 'r')
 print(f.readline())
-print(f.readline())  #当文件较大时需要使用 readline操作文件，相当与一个迭代器
+print(f.readline())  # 当文件较大时需要使用 readline操作文件，相当与一个迭代器
 
 
 for line in f:
